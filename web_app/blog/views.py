dd7694aedf384c5ad7fc3a983887242824512b95
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-31 13:38:02 -0600

    create dummy data and pass to home function

--- web_app/blog/views.py
+++ web_app/blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'blog/home.html') #sub dir of templates/HTML file
+posts = [
+    {
+        'author': 'CoreyMs',
+        'title': 'Blog Post 1',
+        'content': 'First post',
+        'date_post': 'August, 10, 2018' 
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post',
+        'date_post': 'August, 11, 2018' 
+    },
+] #dummy data 
 
+def home(request):
+    #pass dummy data to function by dict
+    #then pass to third arg in render()
+    context = {
+        # 'key': data
+        'posts': posts
+    }
+                        #sub dir of templates/HTML file
+    return render(request, 'blog/home.html', context) 
 
 def about(request):
     return render(request, 'blog/about.html')
