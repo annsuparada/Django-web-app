0cb3f9ac22514734c540a5748cfc0842dd40fa81
Suparada Saitalae <ann.suparada@gmail.com>
2020-01-01 17:50:52 -0600

    create POST request, success message

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username') #if form valid will be in dict
+            messages.success(request, f'Account created for {username}!') # reder success messages
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {"form": form})
