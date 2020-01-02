4e8644ef31a2f5cbdf5b175b2d04d65209704d48
Suparada Saitalae <ann.suparada@gmail.com>
2020-01-01 18:05:58 -0600

    save user to DB

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -6,6 +6,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username') #if form valid will be in dict
             messages.success(request, f'Account created for {username}!') # reder success messages
             return redirect('blog-home') #blog-home is the path name
