a37a831533b1cfaa9c63bf874bad21f9defc1098
Suparada Saitalae <ann.suparada@gmail.com>
2020-01-02 21:04:41 -0600

    resize img

--- web_app/users/models.py
+++ web_app/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image 
 
 class Profile(models.Model): #inherat from User models
     user = models.OneToOneField(User, on_delete=models.CASCADE) #if user get deleted, profile got deleted too
@@ -7,3 +8,15 @@ class Profile(models.Model): #inherat from User models
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save() #save the uploaded img 
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
