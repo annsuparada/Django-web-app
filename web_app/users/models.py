a957295d770da8e50be047a5d48104fb6256b147
Suparada Saitalae <ann.suparada@gmail.com>
2020-01-02 20:21:04 -0600

    add pylint

--- web_app/users/models.py
+++ web_app/users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model): #inherat from user models
+class Profile(models.Model): #inherat from User models
     user = models.OneToOneField(User, on_delete=models.CASCADE) #if user get deleted, profile got deleted too
     image = models.ImageField(default='default.jpg', upload_to='profile_pics') #upload to img directory
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-
